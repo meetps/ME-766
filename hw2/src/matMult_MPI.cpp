306e9da5722e8e00135bb2e87806c8fbbf9a5d78
meetshah1995 <meetshah1995@iitb.ac.in>
2016-03-10 04:54:13 +0530

    Add assig-2

---
+++ hw2/src/matMult_MPI.cpp
@@ -0,0 +1,116 @@
+/***********************************************************
+/ File : Matrix Multiplication using MPI 
+/ Auth ; Meet Pragnesh Shah
+/ Mail : meetshah1995@gmail.com
+/ Desc : HW 2 solution for ME 766  
+/***********************************************************/ 
+
+#include <iostream>
+#include <cstdlib>
+#include <mpi.h>
+#include <time.h>
+
+#define N 1000
+
+using namespace std;
+
+// void display_matrices(int process_id, double* C, double* A, double* B);
+
+int main(int argc, char* argv[])
+{
+	int process_id, no_of_threads;		// Process ID and No of threads
+	int tag = 0;						// Endpoints for time calc
+	double start, end;					// Endpoints for time calc
+	MPI_Status status;    				// Message status
+	
+	double *A, *B, *C;					// Pointers to Matrices
+	int submat_size;					// Size of submatrices
+	int start_address;					// Start address for submatrix
+	
+	MPI_Init(&argc, &argv);								// Init MPI
+	MPI_Comm_rank(MPI_COMM_WORLD, &process_id);			// Start comm chanel
+	MPI_Comm_size(MPI_COMM_WORLD, &no_of_threads);		// Init threads
+
+	
+	submat_size = N/no_of_threads;		 // Sub matrices size 					
+	
+	// Matrix Initialization
+	B = new double [N*N];					
+
+	if(process_id == 0) 
+	{
+		cout << "No. of Threads : " << no_of_threads << endl;	
+		A = new double [N*N];
+		C = new double [N*N];
+		for(int i=0; i<N*N; ++i) 
+		{
+			A[i] = rand()%100;
+			B[i] = rand()%100;
+		}
+	}
+
+	else 
+	{
+		A = new double [N*submat_size];
+		C = new double [N*submat_size];
+		for(int i=0; i<N*submat_size; ++i) 
+		{
+			C[i] = 0.0;
+		}
+	}
+	
+	
+	start = MPI_Wtime();							// Start time
+		
+	if(process_id == 0) 
+	{
+		start_address = N*submat_size;
+		for(int i=1; i<no_of_threads; ++i) 
+		{
+			// Send the input submatrix to salves
+			MPI_Send(A + start_address, N*submat_size, MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
+			MPI_Send(B, N*N, MPI_DOUBLE, i, tag+1, MPI_COMM_WORLD);
+			start_address += N*submat_size;
+		}
+	}
+	else 
+	{
+		// Recceive the input submatrix from master 
+		MPI_Recv(A, N*submat_size, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+		MPI_Recv(B, N*N, MPI_DOUBLE, 0, tag+1, MPI_COMM_WORLD, &status);
+	}
+	
+	// Standard Matrix Multiplication Routine
+	for(int i = 0; i < submat_size; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+			C[j + i*N] = 0;
+			for(int k = 0; k < N; k++)
+			{
+				C[j + i*N] += A[i*N + k]*B[j + N*k];
+				
+			}   
+		}
+	}
+	
+	if(process_id == 0)									 
+	{
+		// Recceive the computed submatrix from slaves to the master 
+		start_address = N*submat_size;
+		for(int i=1; i<no_of_threads; ++i) 
+		{
+			// Send the computed submatrix to master from salves
+			MPI_Recv(C + start_address, N*submat_size, MPI_DOUBLE, i, 2, MPI_COMM_WORLD, &status);
+			start_address += N*submat_size;
+		}
+	}
+	else 
+	{
+		MPI_Send(C, N*submat_size, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+	}
+
+	end = MPI_Wtime();				// Stop time
+	cout << "Time Taken by :" << process_id << " = " << end-start << endl;
+	MPI_Finalize();  		      	// Synchronie the threads
+}
---
+++ hw2/src/matMult_OpenMP.cpp
@@ -0,0 +1,60 @@
+/***********************************************************
+/ File : Matrix Multiplication using OpemMP
+/ Auth ; Meet Pragnesh Shah
+/ Mail : meetshah1995@gmail.com
+/ Desc : HW 2 solution for ME 766  
+/***********************************************************/ 
+
+#include <iostream>
+#include <cstdlib>
+#include <omp.h>
+#include <mpi.h>
+#include <time.h>
+
+#define N 1000                // Size of the matrix
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	float *A, *B, *C;         // Pointers to matrix A,B,C
+	int max_threads;		  // Max number of threads
+	double start, end;		  // Endpoints for time calc
+	size_t matrix_size = N;   // Size for square matrix
+	
+	// Matrix Initialization
+	A = new float[N*N];
+	B = new float[N*N];
+	C = new float[N*N];
+	
+	for(int i=0; i<(N*N); i++)
+	{
+		A[i] = rand()%100;
+		B[i] = rand()%100;
+	}
+
+	start=MPI_Wtime();		   			            // Start stopwatch
+	max_threads = omp_get_max_threads();   			// Get Max threads
+
+	cout<<"Max threads available :"<<max_threads<<endl;
+	
+	//Pragma to parallelize the computation
+	#pragma omp parallel for shared (A, B, C, matrix_size) 
+	
+	//Standard Matrix Multiplication Routine
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+			C[j + i*N] = 0;
+			for(int k = 0; k < N; k++)
+			{
+				C[j + i*N] += A[i*N + k]*B[j + N*k];
+			}    
+		} 
+	}
+	
+	end=MPI_Wtime();				 			    // Stop stopwatch
+	
+	cout<<"Time Taken :"<< (end-start) <<" seconds"<<endl;
+}
